9bb08a7666922cb7894f0475b9d55b0d2a6695d1
Christian Heinemann <ch@chlab.net>
2014-02-01 19:44:29 +0100

    Use ShrinkWrap Descriptors to build ejb-jar.xml

--- src/test/java/net/chlab/sandbox/ejbasync/jms/AsyncMessagingIT.java
+++ src/test/java/net/chlab/sandbox/ejbasync/jms/AsyncMessagingIT.java
@@ -14,7 +14,10 @@ import javax.jms.Queue;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.descriptor.api.Descriptors;
+import org.jboss.shrinkwrap.descriptor.api.ejbjar31.EjbJarDescriptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -32,7 +35,14 @@ public class AsyncMessagingIT {
         return ShrinkWrap.create(WebArchive.class)
                 .addClasses(AsyncMessagingReceiverBean.class, MessagingConstants.class)
                 .addClass(MessageDrivenBeanInterceptor.class)
-                .addAsWebInfResource(AsyncMessagingIT.class.getPackage(), "ejb-jar.xml", "ejb-jar.xml");
+                .addAsWebInfResource(new StringAsset(buildEjbJarDescriptor().exportAsString()), "ejb-jar.xml");
+    }
+
+    private static EjbJarDescriptor buildEjbJarDescriptor() {
+        return Descriptors.create(EjbJarDescriptor.class).getOrCreateAssemblyDescriptor()
+                .getOrCreateInterceptorBinding().ejbName(AsyncMessagingReceiverBean.class.getSimpleName())
+                .interceptorClass(MessageDrivenBeanInterceptor.class.getName()).getOrCreateMethod()
+                .methodName("onMessage").up().up().up();
     }
 
     @Test
